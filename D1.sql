8acbe83e8095832306dfa575889bc7c0eb4c0f6f
DIP-ME <DIP-ME>
2025-01-18 00:50:42 +0530

    dtb nested

---
+++ D1.sql
@@ -0,0 +1,25 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      subs.DM1
+ *
+ * Date Created : Saturday, January 18, 2025 00:50:42
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: `Tester` 
+ */
+
+CREATE TABLE `Tester`
+(
+    `Firstname`  string      NOT NULL,
+    `Lastname`   string      NOT NULL,
+    `Phone`      string      NOT NULL,
+    `Address`    struct<
+        `street`  string,
+        `city`    string
+    >   NOT NULL,
+    `Note`       string      NOT NULL
+)
+;
+
